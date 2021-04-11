d9f83969354ac2cd4793a34a21ad3eb3febe3b42
Reza Ageng <rezaageng5@gmail.com>
2021-04-11 22:18:00 +0700

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -2,6 +2,16 @@
 
 class App {
     public function __construct() {
-        echo "Hmmmm Wangy.....";
+        $url = $this->parseUrl();
+        var_dump($url);
+    }
+
+    public function parseUrl() {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
