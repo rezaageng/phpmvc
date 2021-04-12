81f311f04ec86349362c0e5f55ad6b9444aabf6c
Reza Ageng <rezaageng5@gmail.com>
2021-04-12 15:32:23 +0700

    asset

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,2 +1,6 @@
-
-    <h1>Ganyu Wangy Sekaly</h1>
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Ganyu Wangy Sekaly</h1>
+        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    </div>
+</div>
